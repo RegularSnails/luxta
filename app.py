798ab9b4afd481cebe5f24a6bdcf4a85ce8e522c
lufifraca <lficanolatimer@gmail.com>
2025-09-23 23:19:57 -0700

    Added Flask app with / and /status

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+    return "Hello World from Regular Snails"
+
+@app.get("/status")
+def status():
+    return jsonify(ok=True, team="Regular Snails", msg="My first API")
+
+if __name__ == "__main__":
+    app.run(port=3000)
