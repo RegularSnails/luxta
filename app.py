81a59bef57f2704a49ad876ea0efda1d009c8f8d
Ian Rispin <ianrispin12@gmail.com>
2025-09-23 23:43:52 -0700

    Step 3 code
    Added my code for step 3 of the assignment

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Regular Snails"
 
+@app.get("/")
+def ian():
+    return "This is a super secret message!"
+
 @app.get("/status")
 def status():
     return jsonify(ok=True, team="Regular Snails", msg="My first API")
