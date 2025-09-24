c1c8fe58be3dcd626c1be60552ec365fa0b77584
jtrejoo <159100541+jtrejoo@users.noreply.github.com>
2025-09-23 23:40:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+# Existing endpoints
 @app.get("/")
 def hello():
     return "Hello World from Regular Snails"
@@ -8,6 +9,11 @@ def hello():
 @app.get("/status")
 def status():
     return jsonify(ok=True, team="Regular Snails", msg="My first API")
+
+# add new api function, return api
+@app.get("")
+def my_api():
+    return "This is the new HTTP API function"
 
 if __name__ == "__main__":
     app.run(port=3000)
