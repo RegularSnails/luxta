78a2acfae6be754d8f3d2a62ecaa0d3d27d0e021
jtrejoo <159100541+jtrejoo@users.noreply.github.com>
2025-10-07 23:39:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-# Existing endpoints
 @app.get("/")
 def hello():
     return "Hello World from Regular Snails"
@@ -10,10 +9,25 @@ def hello():
 def status():
     return jsonify(ok=True, team="Regular Snails", msg="My first API")
 
-# add api function
+# api added
 @app.get("/jtrejomyapi")
 def my_api():
     return "This is the new HTTP API function"
+
+# photography assitance type idea 
+def calc_avg_brightness(brightness_values):
+    if not brightness_values:
+        return 0
+    return sum(brightness_values) / len(brightness_values)
+
+@app.get("/brightness")
+def brightnessEndp():
+    # example data, setting up for situation presented
+    
+    values = [100, 180, 200]
+    avg = calc_avg_brightness(values)
+    return jsonify(avg_brightness=avg)
+
 
 if __name__ == "__main__":
     app.run(port=3000)
