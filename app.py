4829e0b8152af531627ca874599b88f7303f0427
aleang01 <leang@cpp.edu>
2025-10-14 21:50:16 -0700

    Create home.html and redirect root

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
 @app.get("/")
-def hello():
-    return "Welcome to Luxta! Under development."
+def home():
+    return render_template('home.html')
 
 @app.get("/status")
 def status():
