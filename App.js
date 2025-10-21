6360999536b3b57378de7560a520a92f5e7e954d
aleang01 <leang@cpp.edu>
2025-10-20 23:04:20 -0700

    Import Leaflet

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import 'leaflet/dist/leaflet.css';
 import HomeScreen from "./src/screens/HomeScreen";
 import LucaScreen from "./src/screens/LucaScreen";
 
