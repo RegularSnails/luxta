2fc6c3f471cf8fd5353d7199e6986da3e57b0d90
lufifraca <lficanolatimer@gmail.com>
2025-10-21 01:54:48 -0700

    Add spring boot server project for CI build

---
+++ server/src/main/java/com/regularsnails/server/AppController.java
@@ -0,0 +1,13 @@
+package com.regularsnails.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping({"/", "/luca"})
+    public String index() {
+        return "index.html";
+    }
+}
+
