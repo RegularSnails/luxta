275d6251cb786522d6d00cab74e528e0458ec898
lufifraca <lficanolatimer@gmail.com>
2025-12-04 11:15:50 -0800

    working log in

--- App.js
+++ App.js
@@ -4,17 +4,22 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import MapScreen from "./src/screens/MapScreen";
 import LoginScreen from "./src/screens/LoginScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import { AuthProvider } from "./src/contexts/AuthContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Regular Snails" }} />
-        <Stack.Screen name="Map" component={MapScreen} options={{ title: "Map" }} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Login" }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Regular Snails" }} />
+          <Stack.Screen name="Map" component={MapScreen} options={{ title: "Map" }} />
+          <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Login" }} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: "Sign Up" }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
